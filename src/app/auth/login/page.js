11d7c81ed70b979b2204db848bfab9cd96a13bb3
cjephuneh <calebjephuneh@gmail.com>
2023-09-28 13:50:23 +0300

    auth

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,12 +1,23 @@
+'use client'
+
 import React from 'react'
+import { useRouter } from 'next/navigation';
+
+
+
 
 function page() {
+    const router = useRouter(); 
+
+    const handleRegisterClick = () => {
+        router.push('/auth/register');
+    }
   return (
      <section className="bg-gray-50 dark:bg-gray-900">
          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-          Flowbite    
+          Bricklabsai    
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -31,11 +42,17 @@ function page() {
                             <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
+                      <a href="/auth/forgotpass" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
                   </div>
                   <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
+                      Don’t have an account yet? 
+                      <span 
+                        onClick={handleRegisterClick} 
+                        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                        Sign up
+                    </span>
                   </p>
               </form>
           </div>
